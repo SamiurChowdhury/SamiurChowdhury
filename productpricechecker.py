bfa19e339cb2a4b70f19bb975ed297d205d3e25e
SamiurChowdhury <samiurchowdhury1@hotmail.com>
2024-12-02 14:24:06 +0000

    Add files via upload

---
+++ productpricechecker.py
@@ -0,0 +1,14 @@
+#assigning prices to both variables
+ferrari = 420000
+toyota = 21000
+
+#conditional statements that checks if one variable is greater than another
+if ferrari>toyota:
+    print("Ferrari is more expensive than Toyota")
+
+elif ferrari<toyota:
+    print("Ferrari is less expensive than Toyota")
+
+else:
+    print("They are both the same value")
+
